6fe391eafd0bca6d02e5eca856332afe1d992fc4
root <root@kidney-PAN4.(none)>
2014-03-11 16:40:31 +0800

    UPDATE:ver0.1,the weibo.cn login script works well:) ADD:add cookie_save_function.

--- src/contrib/weibo_cn/weibo_login.py
+++ src/contrib/weibo_cn/weibo_login.py
@@ -5,28 +5,37 @@
 
 import urllib, urllib2, cookielib
 from bs4 import BeautifulSoup
-
+import os
 
 class Weibo(object):
-
-    def pre_login(self):
+    """return a objcet contains a opener if login successed!
+    use self.opener to get the logined opener.
+    cookiefile saved to cookies.dat by default.
+    start your work from login()
+    """
+    cookiefilename = 'cookies.dat'
+    
+    def build_opener(self):
         #headers = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0' }
         addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0')]
-        self.cookie_jar = cookielib.CookieJar()
+        self.cookie_jar = cookielib.LWPCookieJar()
+        self.opener = urllib2.build_opener(
+                urllib2.HTTPCookieProcessor(self.cookie_jar), 
+                urllib2.HTTPHandler()
+                )
+        self.opener.addheaders = addheaders
+
+    def pre_login(self):
+        self.build_opener()
         pre_login_url = 'http://login.weibo.cn/login/'
         pre_request = urllib2.Request(
                 url = pre_login_url,
                 #headers = self.headers,
                 )
-        opener = urllib2.build_opener(
-                urllib2.HTTPCookieProcessor(self.cookie_jar), 
-                urllib2.HTTPHandler()
-                )
-        opener.addheaders = addheaders
-        pre_response = opener.open(pre_request)
+        pre_response = self.opener.open(pre_request)
         result = pre_response.read()
         pre_response.close()
-        return opener,result
+        return self.opener,result
 
     def get_form(self,html):
         soup = BeautifulSoup(html, 'lxml')
@@ -37,7 +46,19 @@ class Weibo(object):
             data[i.attrs.get('name')] = i.attrs.get('value')
         return login_url,data
 
-    def login(self,username, password):
+    def login(self, username, password, cookiefilename = None):
+        if cookiefilename:
+            if os.path.isfile(cookiefilename):
+                self.build_opener()
+                self.cookie_jar.load(cookiefilename)
+                print 'use existed cookiefile!'
+                return
+        self.real_login(username, password, cookiefilename)
+        
+
+
+    def real_login(self,username, password, cookiefilename):
+        self.cookiefilename = cookiefilename
         self.opener,html = self.pre_login()
         login_url,data = self.get_form(html)
         for key,value in data.items():
@@ -55,11 +76,18 @@ class Weibo(object):
                 )
         result = self.opener.open(request).read()
         if '登录成功' in result:
+            self.write_cookie()
             print 'login succeed!'
         else:
             raise Exception('login fail!Please try again!')
+
+    def write_cookie(self):
         
-                
+        self.cookie_jar.save(self.cookiefilename)
+        
+    def rm_cookiefile(self):
+
+        os.remove(self.cookiefilename)
     
     
 
